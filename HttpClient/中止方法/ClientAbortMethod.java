62b70e2cbd8f2e026d818db85488d63527d06de5
System Administrator <root@xindeMBP.lan>
2019-03-05 16:57:27 +0800

    演示如何在正常完成之前中止HTTP方法

---
+++ HttpClient/中止方法/ClientAbortMethod.java
@@ -0,0 +1,34 @@
+package com.xin;
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+/**
+ * 此示例演示如何在正常完成之前中止HTTP方法。
+ */
+public class ClientAbortMethod {
+
+    public final static void main(String[] args) throws Exception {
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        try {
+            HttpGet httpget = new HttpGet("http://httpbin.org/get");
+
+            System.out.println("Executing request " + httpget.getURI());
+            CloseableHttpResponse response = httpclient.execute(httpget);
+            try {
+                System.out.println("----------------------------------------");
+                System.out.println(response.getStatusLine());
+                // Do not feel like reading the response body
+                // Call abort on the request object
+                httpget.abort();
+            } finally {
+                response.close();
+            }
+        } finally {
+            httpclient.close();
+        }
+    }
+
+}
